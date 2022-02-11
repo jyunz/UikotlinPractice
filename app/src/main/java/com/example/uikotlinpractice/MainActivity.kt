a9018e20954e477b038889bd99af5040b93f0bdf
Roy <J.jyun0525@gmail.com>
2022-02-11 10:16:17 +0900

    입력 키보드가 왜 영문만 가능한지 의문

--- app/src/main/java/com/example/uikotlinpractice/MainActivity.kt
+++ app/src/main/java/com/example/uikotlinpractice/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.uikotlinpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        okBtn.setOnClickListener {
+
+//            입력된 내용? : contentEdt의 text의 값. 조회 (get) 변수에 저장해두자.
+
+            val inputContent = contentEdt.text.toString()
+
+//            저장해둔 입력값을 텍스트 뷰의 내용으로 반영.(set)
+
+            resultTxt.text = inputContent
+
+        }
     }
 }
